1aabc2c7058c6d1bf5b44bdf4751fb91aecb65a9
honggu <wckhg89@gmail.com>
2016-12-25 20:55:18 +0900

    init

---
+++ src/main/java/com/zumgu/toy/ToyProjectApplication.java
@@ -0,0 +1,12 @@
+package com.zumgu.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProjectApplication.class, args);
+	}
+}
